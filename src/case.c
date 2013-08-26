da4cbcb3a2c62b14d53d6b7217057b7eeedf749d
Johannes Auer <johannes.auer@me.com>
2013-08-26 08:57:15 +0200

    add case imp

--- src/case.c
+++ src/case.c
@@ -1,21 +1,51 @@
 #include "case.h"
 
-struct Case {
-    Type *type;
-};
-
 typedef struct {
-    Integer32 match;
-    Integer32 guard;
-    Integer32 value;
+    VALUE *match;
+    VALUE *guard;
+    VALUE *value;
 } Clause;
 
-typedef struct {
-    Integer8 length;
-    Integer32 arg;
-    Clause clause[];
-} Model;
+struct Case {
+    Type *type;
+    Integer32 count;
+    Clause clauses[];
+};
+
+static Case *CaseCreate(Integer32 count, VALUE **error) {
+    Case *block = MemoryAlloc(sizeof(Case) + sizeof(Clause) * count, error);
+    if (*error != NULL) {
+        return NULL;
+    }
+    block->type = TypeCase;
+    block->count = count;
+    return block;
+}
 
 VALUE *CaseDecode(Byte **bytes, VALUE **error) {
+    Integer32 count = DecodeInteger32VLE(bytes);
+    Case *block = CaseCreate(count, error);
+    if (*error != NULL) {
+        goto returnError;
+    }
+    for (Integer32 index = 0; index < count; index += 1) {
+        block->clauses[index].match = DecodeValue(bytes, error);
+        if (*error != NULL) {
+            goto deallocCase;
+        }
+        block->clauses[index].guard = DecodeValue(bytes, error);
+        if (*error != NULL) {
+            goto deallocCase;
+        }
+        block->clauses[index].value = DecodeValue(bytes, error);
+        if (*error != NULL) {
+            goto deallocCase;
+        }
+    }
+    return BridgeFromCase(block);
+
+deallocCase:
+    MemoryDealloc(block);
+returnError:
     return NULL;
 }
