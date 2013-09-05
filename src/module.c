09d60d9488641da2a0757abe584d27e6b82885fe
Johannes Auer <johannes.auer@me.com>
2013-09-05 12:21:41 +0200

    add module decode imp

--- src/module.c
+++ src/module.c
@@ -7,7 +7,7 @@ typedef struct Scope Scope;
 
 struct Module {
     VALUE *type;
-    Scope *root;
+    Scope *scope;
 };
 
 struct Definition {
@@ -22,15 +22,55 @@ struct Scope {
 };
 
 /* private */
-static Module *ModuleCreate(Error *error) {
+static Module *ModuleCreate(Scope *scope, Error *error) {
     Module *module = MemoryAlloc(sizeof(Module), error);
     if (*error != ErrorNone) return NULL;
     module->type = RuntimeValueForConstant(ConstantModuleType);
+    module->scope = scope;
     return module;
+}
+
+static Scope *ModuleCreateScope(Integer32 count, Error *error) {
+    Scope *scope = MemoryAlloc(sizeof(Scope) + sizeof(Definition) * count, error);
+    if (*error != ErrorNone) return NULL;
+    scope->count = count;
+    return scope;
+}
+
+static void ModuleDeallocScope(Scope *scope) {
+    MemoryDealloc(scope);
+}
+
+static Scope *ModuleDecodeScope(Byte **bytes, Error *error) {
+    Integer32 count = DecodeInteger32(bytes);
+    Scope *scope = ModuleCreateScope(count, error);
+    if (*error != ErrorNone) goto returnError;
+    for (Integer32 index = 0; index < count; index += 1) {
+        scope->definitions[index].variable = DecodeValue(bytes, error);
+        if (*error != ErrorNone) goto deallocScope;
+        scope->definitions[index].value = DecodeValue(bytes, error);
+        if (*error != ErrorNone) goto deallocScope;
+        scope->definitions[index].local = ModuleDecodeScope(bytes, error);
+        if (*error != ErrorNone) goto deallocScope;
+    }
+    return scope;
+
+deallocScope:
+    ModuleDeallocScope(scope);
+returnError:
+    return NULL;
 }
 
 /* public */
 VALUE *ModuleDecode(Byte **bytes, Error *error) {
-    Module *module = ModuleCreate(error);
+    Scope *scope = ModuleDecodeScope(bytes, error);
+    if (*error != ErrorNone) goto returnError;
+    Module *module = ModuleCreate(scope, error);
+    if (*error != ErrorNone) goto deallocScope;
     return module;
+
+deallocScope:
+    ModuleDeallocScope(scope);
+returnError:
+    return NULL;
 }
