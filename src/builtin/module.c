f39f9e71009d5f67dad83b4e60aa90607e388be9
Johannes Auer <johannes.auer@me.com>
2013-09-18 11:02:11 +0200

    ...

--- src/builtin/module.c
+++ src/builtin/module.c
@@ -16,7 +16,7 @@ struct Module {
 static Size ModuleSize(Integer32 count) {
     return sizeof(Module) + sizeof(Definition) * count;
 }
-/*
+
 static Module *ModuleCreate(Integer32 count) {
     Size size = ModuleSize(count);
     Module *module = MemoryAlloc(size);
@@ -25,7 +25,7 @@ static Module *ModuleCreate(Integer32 count) {
     module->count = count;
     return module;
 }
-*/
+
 static Size ModuleDealloc(Module *module, Integer32 count) {
     Size size = ModuleSize(module->count);
     for (Integer32 index = 0; index < count; index += 1)
@@ -34,8 +34,7 @@ static Size ModuleDealloc(Module *module, Integer32 count) {
     return size;
 }
 
-/*
-Value *ModuleDecode(Byte **bytes) {
+static Module *ModuleDecodeRecursive(Byte **bytes) {
     Integer32 count = DecodeInteger32(bytes);
     Module *module = ModuleCreate(count);
     if (module == NULL)
@@ -47,23 +46,41 @@ Value *ModuleDecode(Byte **bytes) {
         Value *value = ValueDecode(bytes);
         if (value == NULL)
             return ModuleDealloc(module, index), NULL;
-        Module *local = ModuleDecode(bytes);
+        Module *local = ModuleDecodeRecursive(bytes);
         if (local == NULL)
             return ModuleDealloc(module, index), NULL;
         module->definitions[index].name = name;
         module->definitions[index].value = value;
         module->definitions[index].local = local;
     }
+    return module;
+}
+
+Value *ModuleDecode(Byte **bytes) {
+    Module *module = ModuleDecodeRecursive(bytes);
     return ValueCreate(MODEL_MODULE, module);
 }
-*/
 
 Value *ModuleEval(Module *module, Value *context) {
-    /* missing */
+    for (Integer32 index = 0; index < module->count; index += 1) {
+        context = ValueSetValueForKey(context, module->definitions[index].value, module->definitions[index].name);
+        if (context == NULL)
+            return NULL;
+    }
+    Module *new = ModuleCreate(module->count);
+    if (new == NULL)
+        return NULL;
+    for (Integer32 index = 0; index < module->count; index += 1) {
+        Value *value = ValueEval(module->definitions[index].value, context);
+        if (value == NULL)
+            return /**/ NULL;
+        new->definitions[index].name = module->definitions[index].name;
+        new->definitions[index].value = value;
+    }
+    return ValueCreate(MODEL_MODULE, new);
 }
 
 Value *ModuleEqual(Module *module, Module *other) {
-    /* missing */
 }
 
 Size ModuleRelease(Module *module) {
