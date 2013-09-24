074738973313dfcd44e58034a2996d669890d0a4
Johannes Auer <johannes.auer@me.com>
2013-09-24 16:47:16 +0200

    add encode decode imps

--- src/decode.c
+++ src/decode.c
@@ -1,20 +1,46 @@
 #include "decode.h"
 
 Integer8 DecodeInteger8(Byte **bytes) {
+    Integer8 value = **bytes;
+    *bytes += sizeof(Integer8);
+    return value;
 }
 
 Integer16 DecodeInteger16(Byte **bytes) {
+    Byte *blocks = *bytes;
+    Integer16 value = blocks[0];
+    value = value << 8 | blocks[1];
+    *bytes += sizeof(Integer16);
+    return value;
 }
 
 Integer32 DecodeInteger32(Byte **bytes) {
+    Byte *blocks = *bytes;
+    Integer32 value = blocks[0];
+    value = value << 8 | blocks[1];
+    value = value << 8 | blocks[2];
+    value = value << 8 | blocks[3];
+    *bytes += sizeof(Integer32);
+    return value;
 }
 
 Integer64 DecodeInteger64(Byte **bytes) {
+    Byte *blocks = *bytes;
+    Integer64 value = blocks[0];
+    value = value << 8 | blocks[1];
+    value = value << 8 | blocks[2];
+    value = value << 8 | blocks[3];
+    value = value << 8 | blocks[4];
+    value = value << 8 | blocks[5];
+    value = value << 8 | blocks[6];
+    value = value << 8 | blocks[7];
+    *bytes += sizeof(Integer64);
+    return value;
 }
 
 Float64 DecodeFloat64(Byte **bytes) {
     Integer64 binary = DecodeInteger64(bytes);
     Float64 value = 0;
-    MemoryCopy(&binary, &value, 8);
+    MemoryCopy(&binary, &value, sizeof(Integer64));
     return value;
 }
--- src/encode.c
+++ src/encode.c
@@ -1,16 +1,41 @@
 #include "encode.h"
 
 void EncodeInteger8(Integer8 value, Byte **bytes) {
+    **bytes = value;
+    *bytes += sizeof(Integer8);
 }
 
 void EncodeInteger16(Integer16 value, Byte **bytes) {
+    Byte *blocks = *bytes;
+    blocks[0] = value >> 8 & 0XFF;
+    blocks[1] = value & 0XFF;
+    *bytes += sizeof(Integer16);
 }
 
 void EncodeInteger32(Integer32 value, Byte **bytes) {
+    Byte *blocks = *bytes;
+    blocks[0] = value >> 24 & 0XFF;
+    blocks[1] = value >> 16 & 0XFF;
+    blocks[2] = value >> 8 & 0XFF;
+    blocks[3] = value & 0XFF;
+    *bytes += sizeof(Integer32);
 }
 
 void EncodeInteger64(Integer64 value, Byte **bytes) {
+    Byte *blocks = *bytes;
+    blocks[0] = value >> 56 & 0XFF;
+    blocks[1] = value >> 48 & 0XFF;
+    blocks[2] = value >> 40 & 0XFF;
+    blocks[3] = value >> 32 & 0XFF;
+    blocks[4] = value >> 24 & 0XFF;
+    blocks[5] = value >> 16 & 0XFF;
+    blocks[6] = value >> 8 & 0XFF;
+    blocks[7] = value & 0XFF;
+    *bytes += sizeof(Integer64);
 }
 
 void EncodeFloat64(Float64 value, Byte **bytes) {
+    Integer64 binary = 0;
+    MemoryCopy(&value, &binary, sizeof(Integer64));
+    EncodeInteger64(binary, bytes);
 }
