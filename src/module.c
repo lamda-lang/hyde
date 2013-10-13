0c658c0388ec03ff93a8a4debcb0bb2de043a056
Johannes Auer <johannes.auer@me.com>
2013-10-13 13:27:55 +0200

    minor fix

--- src/module.c
+++ src/module.c
@@ -11,7 +11,11 @@ struct Module {
     Definition definitions[];
 };
 
-static Module *ModuleDecode(Binary *binary, Integer32 *offset);
+static Module *ModuleCreate(Integer32 count) {
+    Module *module = MemoryAllocRegion(sizeof(Module), sizeof(Definition), count);
+    module->count = count;
+    return module;
+}
 
 static void ModuleDealloc(Module *module, Integer32 count) {
     for (Integer32 index = 0; index < count; index += 1) {
@@ -21,59 +25,32 @@ static void ModuleDealloc(Module *module, Integer32 count) {
     MemoryDealloc(module);
 }
 
-static Module *ModuleCreate(Integer32 count) {
-    Module *module = MemoryAllocRegion(sizeof(Module), sizeof(Definition), count);
-    module->count = count;
-    return module;
-}
-
-static Definition *ModuleCreateDefinition(Value *name, Value *value, Module *local) {
-    Definition *definition = MemoryAllocUnit(sizeof(Definition));
-    definition->name = name;
-    definition->value = value;
-    definition->local = local;
-    return definition;
-}
-
-static Definition *ModuleDecodeDefinition(Binary *binary, Integer32 *offset) {
-    Value *name = BinaryDecodeValue(binary, offset);
-    if (name == NULL)
-        return NULL;
-    Value *value = BinaryDecodeValue(binary, offset);
-    if (value == NULL)
-        return NULL;
-    Module *module = ModuleDecode(binary, offset);
-    if (module == NULL)
-        return NULL;
-    Definition *definition = ModuleCreateDefinition(name, value, module);
-    if (definition == NULL)
-        goto out;
-    return definition;
-
-out:
-    ModuleDealloc(module, module->count);
-    return NULL;
-}
-
 static Module *ModuleDecode(Binary *binary, Integer32 *offset) {
-    /*
     Integer32 count;
     if (!BinaryDecodeInteger32(binary, offset, &count))
         return NULL;
     Module *module = ModuleCreate(count);
     Integer32 index = 0;
     while (index < count) {
-        Definition *definition = ModuleDecodeDefinition(binary, offset);
-        if (definition == NULL)
+        Value *name = BinaryDecodeValue(binary, offset);
+        if (name == NULL)
             goto out;
-        module->definitions[index] = definition;
+        Value *value = BinaryDecodeValue(binary, offset);
+        if (value == NULL)
+            goto out;
+        Module *local = ModuleDecode(binary, offset);
+        if (module == NULL)
+            goto out;
+        module->definitions[index].name = name;
+        module->definitions[index].value = value;
+        module->definitions[index].local = local;
         index += 1;
     }
     return module;
 
 out:
     ModuleDealloc(module, index);
-   */ return NULL;
+    return NULL;
 }
 
 Value *ModuleDecodePrimitive(Binary *binary, Integer32 *offset) {
